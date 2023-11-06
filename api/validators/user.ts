1167784e35a3cb1c024bd6c70e1f59805fc00867
yUnreal <unrealwebdev@gmail.com>
2023-11-06 14:43:03 -0300

    feat: new validators

--- api/validators/user.ts
+++ api/validators/user.ts
@@ -1,5 +1,5 @@
-import { number, object, string } from "yup";
-import { NotificationType } from "../typings/types";
+import { array, boolean, number, object, string } from "yup";
+import { NotificationType, TeamPermissions } from "../typings/types";
 
 export const createNotificationValidator = object({
     content: string().required(),
@@ -13,3 +13,53 @@ export const createNotificationValidator = object({
         .required(),
     url: string().url(),
 }).strict();
+
+export const createTeamValidator = object({
+    members: array(
+        object({
+            id: string()
+                .matches(/^\d{16,21}$/)
+                .required(),
+            permission: number()
+                .oneOf([
+                    TeamPermissions.Administrator,
+                    TeamPermissions.ReadOnly,
+                ])
+                .required(),
+            owner: boolean(),
+        })
+    )
+        .required()
+        .test(
+            "one-owner",
+            "Some member of the team must be the owner",
+            (members) => members.filter((member) => member.owner).length === 1
+        ),
+    name: string().required().min(3).max(15).required(),
+    avatar_url: string().required(),
+    description: string().min(5).max(50),
+    bot_id: string()
+        .matches(/^\d{16,21}$/)
+        .required(),
+}).required();
+
+export const updateTeamValidator = object({
+    name: string().min(3).max(15),
+    avatar_url: string(),
+    description: string().min(5).max(50),
+    members: array(
+        object({
+            id: string().matches(/^\d{16,21}$/),
+            permission: number().oneOf([
+                TeamPermissions.Administrator,
+                TeamPermissions.ReadOnly,
+            ]),
+            owner: boolean(),
+        })
+    ),
+})
+    .test("at-least-one-key", "You must pass at least one key", (obj) => {
+        return Object.keys(obj).length > 0;
+    })
+    .noUnknown()
+    .strict();
