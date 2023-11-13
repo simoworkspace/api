958ad022817aeb62bcb93ec208855665d5a14ce0
yUnreal <unrealwebdev@gmail.com>
2023-11-13 14:10:29 -0300

    feat: returning author with username, avatar and id

--- api/helpers/fetchFeedbacks.ts
+++ api/helpers/fetchFeedbacks.ts
@@ -2,6 +2,7 @@ import { HttpStatusCode } from "axios";
 import { FEEDBACK } from "./errors.json";
 import type { Request, Response } from "express";
 import { feedbackSchema } from "../models/Feedback";
+import { userSchema } from "../models/User";
 
 /**
  * Fetches all feedbacks from a bot
@@ -16,5 +17,34 @@ export const fetchFeedbacks = async (req: Request, res: Response) => {
     if (!data || data.length < 1)
         return res.status(HttpStatusCode.NoContent).json(FEEDBACK.NO_FEEDBACKS);
 
-    return res.status(HttpStatusCode.Ok).json(data);
+    return Promise.all(
+        data.map(
+            async ({
+                stars,
+                author_id,
+                content,
+                target_bot,
+                edited,
+                reply_message,
+            }) => {
+                const author = await userSchema.findById(author_id, {
+                    username: 1,
+                    avatar: 1,
+                });
+
+                return {
+                    stars,
+                    content,
+                    target_bot,
+                    edited,
+                    reply_message,
+                    author: {
+                        username: author?.username,
+                        avatar: author?.avatar,
+                        id: author_id,
+                    },
+                };
+            }
+        )
+    ).then((result) => res.status(HttpStatusCode.Ok).json(result));
 };
