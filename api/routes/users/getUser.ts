55351e1ac148ff059762d8d0921ce920ce1715e3
yUnreal <unrealwebdev@gmail.com>
2023-11-14 20:17:17 -0300

    feat: now is possible to get any user from db

--- api/routes/users/getUser.ts
+++ api/routes/users/getUser.ts
@@ -9,15 +9,17 @@ import { getUserId } from "../../helpers/getUserId";
  * Gets a user notifications or a user from Discord API
  */
 export const getUser = async (req: Request, res: Response) => {
-    const userId = await getUserId(req.headers);
+    const { method } = req.params;
+    const authorId = await getUserId(req.headers);
 
-    if (req.params.method === "notifications")
-        return fetchUserNotifications(res, userId);
+    if (method === "notifications")
+        return fetchUserNotifications(res, authorId);
 
-    const user = await userSchema.findById(userId, { __v: 0 });
+    const user = await userSchema.findById(method, { __v: 0 });
 
     if (!user)
         return res.status(HttpStatusCode.NotFound).json(USER.UNKNOWN_USER);
+    if (method !== authorId) user.notifications = null as any;
 
     return res.status(HttpStatusCode.Ok).json(user);
 };
