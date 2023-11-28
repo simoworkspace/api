1cb07db27d52d62dcf10215d1847329e840d13e1
yUnreal <unrealwebdev@gmail.com>
2023-11-28 15:33:03 -0300

    feat: new features comming

---
+++ api/models/AuditLog.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+import type { AuditLogStructure } from "../typings/types";
+
+const rawAuditLogSchema = new Schema<AuditLogStructure>({
+    team_id: String,
+    executor_id: String,
+    created_at: String,
+    id: String,
+    action_type: Number,
+});
+
+export const auditLogModel = model("auditLog", rawAuditLogSchema);
