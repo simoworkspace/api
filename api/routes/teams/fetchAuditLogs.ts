88bbca39d72bf56beedc181fba2778eb7ef7d525
yUnreal <unrealwebdev@gmail.com>
2023-12-07 16:37:59 -0300

    refactor: returnig _id, avatar and username when fetching audit-logs

--- api/routes/teams/fetchAuditLogs.ts
+++ api/routes/teams/fetchAuditLogs.ts
@@ -4,6 +4,7 @@ import { HttpStatusCode } from "axios";
 import { TEAM } from "../../utils/errors.json";
 import { TeamPermissions } from "../../typings/types";
 import { auditLogModel } from "../../models/AuditLog";
+import { userModel } from "../../models/User";
 
 export const fetchAuditLogs = async (
     res: Response,
@@ -31,5 +32,22 @@ export const fetchAuditLogs = async (
         { _id: 0 }
     );
 
-    return res.status(HttpStatusCode.Ok).json(auditLogs);
+    if (!auditLogs)
+        return res
+            .status(HttpStatusCode.NotFound)
+            .json(TEAM.UNKNOWN_AUDIT_LOGS);
+
+    const users = await userModel.find({}, { username: 1, avatar: 1, _id: 1 });
+
+    return res.status(HttpStatusCode.Ok).json({
+        ...auditLogs._doc,
+        entries: auditLogs?.entries.map((entry) => {
+            const executor = users.find(
+                (user) => user._id === entry.executor_id
+            );
+            const target = users.find((user) => user._id === entry.target_id);
+
+            return { ...entry, executor, target };
+        }),
+    });
 };
