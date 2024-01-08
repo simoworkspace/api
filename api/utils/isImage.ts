9a4c744748894140b1b327c079a24a486f4cdf4e
Unreal <unrealwebdev@gmail.com>
2024-01-07 22:10:32 -0300

    chore: removing dumb console.log

--- api/utils/isImage.ts
+++ api/utils/isImage.ts
@@ -2,7 +2,5 @@ export const isImage = async (url: string) => {
     const response = await fetch(url);
     const contentType = response.headers.get("content-type");
 
-    console.log(contentType);
-
     return contentType ? contentType.startsWith("image") : false;
 };
